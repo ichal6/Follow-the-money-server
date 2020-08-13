7a9ed6e4dfb698b624a6830a9861d6ab55f8ea07
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-08-13 14:34:56 +0200

    Initial commit

---
+++ src/main/java/com/example/followthemoney/FollowTheMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.example.followthemoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowTheMoneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FollowTheMoneyApplication.class, args);
+    }
+
+}
