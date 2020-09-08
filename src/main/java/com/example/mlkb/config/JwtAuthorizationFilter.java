a3b5d7d2f56715fb9e42641633d92b8e6b070c09
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-09-08 18:06:20 +0200

    Implement get token via cookie

--- src/main/java/com/example/mlkb/config/JwtAuthorizationFilter.java
+++ src/main/java/com/example/mlkb/config/JwtAuthorizationFilter.java
@@ -11,6 +11,7 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -32,27 +33,58 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain) throws IOException, ServletException {
-        UsernamePasswordAuthenticationToken authentication = getAuthentication(request);
+        UsernamePasswordAuthenticationToken authentication = getAuthentication(request, response, filterChain);
         if (authentication == null) {
-            filterChain.doFilter(request, response);
+            //filterChain.doFilter(request, response);
             return;
         }
         SecurityContextHolder.getContext().setAuthentication(authentication);
         filterChain.doFilter(request, response);
     }
 
-    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request, HttpServletResponse response,
+                                                                  FilterChain filterChain) throws IOException, ServletException {
         String token = request.getHeader(TOKEN_HEADER);
-        if (token != null && token.startsWith(TOKEN_PREFIX)) {
+        if(token == null){
+            token = getTokenFromCookie(request, response, filterChain);
+            logger.info(token);
+        }
+        if (token != null) {
+            if(token.startsWith(TOKEN_PREFIX)){
+                token = token.replace(TOKEN_PREFIX, "");
+            }
             String userName = JWT.require(Algorithm.HMAC256(secret))
                     .build()
-                    .verify(token.replace(TOKEN_PREFIX, ""))
+                    .verify(token)
                     .getSubject();
             if (userName != null) {
                 UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
                 return new UsernamePasswordAuthenticationToken(userDetails.getUsername(), null, userDetails.getAuthorities());
             }
         }
+
         return null;
+    }
+
+    private String getTokenFromCookie(HttpServletRequest request, HttpServletResponse response,
+                                      FilterChain chain) throws IOException, ServletException {
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null || cookies.length == 0) {
+            chain.doFilter(request, response);
+            return null;
+        }
+
+        Cookie tokenCookie = null;
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                tokenCookie = cookie;
+            }
+        }
+
+        if (tokenCookie == null) {
+            chain.doFilter(request, response);
+            return null;
+        }
+        return tokenCookie.getValue();
     }
 }
