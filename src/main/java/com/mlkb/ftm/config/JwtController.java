011cb0c16d5d899306e48dd038ef72362003d94f
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-09-14 16:41:58 +0200

    Correct logout option

--- src/main/java/com/mlkb/ftm/config/JwtController.java
+++ src/main/java/com/mlkb/ftm/config/JwtController.java
@@ -5,19 +5,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class JwtController {
     @GetMapping("/logoutUser")
-    public void logout(HttpServletResponse response, HttpServletRequest request) {
+    public void logout(HttpServletResponse response) {
+        removeJwtCookie(response);
+        removeEmailCookie(response);
+
+        SecurityContextHolder.getContext().setAuthentication(null);
+    }
+
+    private void removeEmailCookie(HttpServletResponse response){
+        Cookie cookie = new Cookie("e-mail", null);
+        //TODO: When in production must do cookie.setSecure(true);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+    }
+
+    private void removeJwtCookie(HttpServletResponse response){
         Cookie cookie = new Cookie("token", null);
         cookie.setHttpOnly(true);
         //TODO: When in production must do cookie.setSecure(true);
         cookie.setMaxAge(0);
         response.addCookie(cookie);
-        SecurityContextHolder.getContext().setAuthentication(null);
     }
 
     @GetMapping("/isLogin")
