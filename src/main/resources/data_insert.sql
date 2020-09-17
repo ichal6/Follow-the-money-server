fbc235aff64f4f59e02faf1be2bb504f8c597723
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-09-17 12:53:36 +0200

    Update data insert

--- src/main/resources/data_insert.sql
+++ src/main/resources/data_insert.sql
@@ -27,3 +27,18 @@ VALUES (default, 'CASH', 'USD', -1000.0, 'WALLET MY', 200.0, 1);
 
 INSERT INTO "account" (id, account_type, currency, current_balance, name, starting_balance, user_id)
 VALUES (default, 'BANK', 'USD', 4000.0, 'PEKAO SA', 300.0, 1);
+
+INSERT INTO "category" (id, general_type, name, user_id)
+VALUES (default, 'EXPENSE', 'Education', '1');
+
+INSERT INTO "subcategory" (id, name, type, category_id)
+VALUES (default, 'studies', 'EXPENSE', 1);
+
+INSERT INTO "payee" (id, general_type, name, user_id)
+VALUES (default, 'INCOME', 'Job', 1);
+
+INSERT INTO "transaction" (id, date, title, type, value, category_id, payee_id, account_id)
+VALUES (default, TO_DATE('2020-09-02','YYYY-MM-DD'), 'buy car', 'EXPENSE', 2500.00, 1, 1, 1);
+
+INSERT INTO "transfer" (id, date, title, value, account_from_id, account_to_id)
+VALUES (default, TO_DATE('2019-09-02','YYYY-MM-DD'), 'from friend', 280.0, 2, 1);
