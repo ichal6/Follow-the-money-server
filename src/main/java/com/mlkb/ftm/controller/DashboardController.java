4a86b1ccb2c39dd45d575222f0c2d4025952114a
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-09-27 18:47:46 +0200

    Delete unused imports

--- src/main/java/com/mlkb/ftm/controller/DashboardController.java
+++ src/main/java/com/mlkb/ftm/controller/DashboardController.java
@@ -9,9 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.rmi.NoSuchObjectException;
-import java.util.Optional;
-
 @RestController
 @RequestMapping("/api/dashboard")
 public class DashboardController {
