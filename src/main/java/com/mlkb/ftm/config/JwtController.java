ebe75637ea7cc4354993062d23fe6a2d3eeceeeb
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-09-28 19:10:34 +0200

    Add swagger API

--- src/main/java/com/mlkb/ftm/config/JwtController.java
+++ src/main/java/com/mlkb/ftm/config/JwtController.java
@@ -2,6 +2,8 @@ package com.mlkb.ftm.config;
 
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.Cookie;
@@ -9,6 +11,11 @@ import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class JwtController {
+
+    @PostMapping("/login")
+    public void login(@RequestBody LoginCredentials credentials) {
+    }
+
     @GetMapping("/logoutUser")
     public void logout(HttpServletResponse response) {
         removeJwtCookie(response);
