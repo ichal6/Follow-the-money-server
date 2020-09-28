8599fa99d46a09040d39274efb7c91b0bfa321fe
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-09-28 20:20:50 +0200

    Add check for authorities

--- src/main/java/com/mlkb/ftm/controller/DashboardController.java
+++ src/main/java/com/mlkb/ftm/controller/DashboardController.java
@@ -4,6 +4,8 @@ import com.mlkb.ftm.modelDTO.DashboardDTO;
 import com.mlkb.ftm.service.DashboardService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,11 +22,22 @@ public class DashboardController {
 
     @GetMapping("/{email}")
     public ResponseEntity<Object> getDashboard(@PathVariable("email") String email){
+        if(!checkPermit(email)){
+            return new ResponseEntity<>("Access Denied", HttpStatus.FORBIDDEN);
+        }
         try {
             DashboardDTO dashboardDTO = dashboardService.getDashboard(email);
             return new ResponseEntity<>(dashboardDTO, HttpStatus.OK);
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+    private boolean checkPermit(String email) {
+        String emailFromToken = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if(emailFromToken == null){
+            return false;
+        }
+        return email.equals(emailFromToken);
     }
 }
