2a4695536d858e00e5526ef0ee55b173e620285b
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-09-30 13:13:47 +0200

    Add check permit for CategoryController

--- src/main/java/com/mlkb/ftm/controller/CategoryController.java
+++ src/main/java/com/mlkb/ftm/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.mlkb.ftm.controller;
 import com.mlkb.ftm.modelDTO.CategoryDTO;
 import com.mlkb.ftm.modelDTO.SubcategoryDTO;
 import com.mlkb.ftm.service.CategoryService;
+import com.mlkb.ftm.validation.AccessValidator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,13 +14,16 @@ import java.util.List;
 @RequestMapping("/api/category")
 public class CategoryController {
     private final CategoryService categoryService;
+    private final AccessValidator accessValidator;
 
-    public CategoryController(CategoryService categoryService){
+    public CategoryController(CategoryService categoryService, AccessValidator validator){
         this.categoryService = categoryService;
+        this.accessValidator = validator;
     }
 
     @GetMapping("/{email}")
     public ResponseEntity<Object> getCategories(@PathVariable String email){
+        accessValidator.checkPermit(email);
         try {
             List<CategoryDTO> categoriesDTO = categoryService.getCategories(email);
             return new ResponseEntity<>(categoriesDTO, HttpStatus.OK);
@@ -30,6 +34,7 @@ public class CategoryController {
 
     @GetMapping("/expense/{email}")
     public ResponseEntity<Object> getCategoriesForExpense(@PathVariable String email){
+        accessValidator.checkPermit(email);
         try {
             List<CategoryDTO> categoriesDTO = categoryService.getCategoriesForExpense(email);
             return new ResponseEntity<>(categoriesDTO, HttpStatus.OK);
@@ -40,6 +45,7 @@ public class CategoryController {
 
     @GetMapping("/income/{email}")
     public ResponseEntity<Object> getCategoriesForIncome(@PathVariable String email){
+        accessValidator.checkPermit(email);
         try {
             List<CategoryDTO> categoriesDTO = categoryService.getCategoriesForIncome(email);
             return new ResponseEntity<>(categoriesDTO, HttpStatus.OK);
@@ -50,6 +56,7 @@ public class CategoryController {
 
     @DeleteMapping("/{email}/{id}")
     public ResponseEntity<Object> deleteCategory(@PathVariable String email, @PathVariable Long id){
+        accessValidator.checkPermit(email);
         try {
             categoryService.deleteCategory(email, id);
             return new ResponseEntity<>(null, HttpStatus.OK);
@@ -61,6 +68,7 @@ public class CategoryController {
     @DeleteMapping("/{email}/{idCat}/{idSub}")
     public ResponseEntity<Object> deleteCategory(@PathVariable String email, @PathVariable Long idCat,
                                                  @PathVariable Long idSub){
+        accessValidator.checkPermit(email);
         try {
             categoryService.deleteSubcategory(email, idCat, idSub);
             return new ResponseEntity<>(null, HttpStatus.OK);
@@ -72,6 +80,7 @@ public class CategoryController {
     @PutMapping("/{email}/{id}")
     public ResponseEntity<Object> UpdateCategory(@PathVariable String email, @PathVariable Long id,
                                                  @RequestBody String newName){
+        accessValidator.checkPermit(email);
         try {
             categoryService.updateCategory(email, id, newName);
             return new ResponseEntity<>(null, HttpStatus.CREATED);
@@ -84,6 +93,7 @@ public class CategoryController {
     public ResponseEntity<Object> UpdateSubcategory(@PathVariable String email, @PathVariable Long idCat,
                                                  @PathVariable Long idSub,
                                                  @RequestBody String newName){
+        accessValidator.checkPermit(email);
         try {
             categoryService.updateSubcategory(email, idCat, idSub, newName);
             return new ResponseEntity<>(null, HttpStatus.CREATED);
@@ -94,6 +104,7 @@ public class CategoryController {
 
     @PostMapping("/{email}")
     public ResponseEntity<Object> AddCategory(@PathVariable String email, @RequestBody CategoryDTO newCategory){
+        accessValidator.checkPermit(email);
         try {
             categoryService.addCategory(email, newCategory);
             return new ResponseEntity<>(null, HttpStatus.CREATED);
@@ -105,6 +116,7 @@ public class CategoryController {
     @PostMapping("/{email}/{id}")
     public ResponseEntity<Object> AddCategory(@PathVariable String email, @PathVariable Long id,
                                               @RequestBody SubcategoryDTO newSubcategory){
+        accessValidator.checkPermit(email);
         try {
             categoryService.addSubcategory(email, id, newSubcategory);
             return new ResponseEntity<>(null, HttpStatus.CREATED);
