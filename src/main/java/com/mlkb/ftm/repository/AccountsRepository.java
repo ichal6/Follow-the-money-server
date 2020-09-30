493c9ea47a75840d3360c6a1fc34aa80475faf74
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-09-30 12:01:21 +0200

    Remove accounts repository

--- src/main/java/com/mlkb/ftm/repository/AccountsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mlkb.ftm.repository;
-
-import com.mlkb.ftm.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountsRepository extends JpaRepository<Account, Long> {
-}
