99dd467dde5264d36ff004fa8742d745e4ebfc51
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-10-15 12:03:09 +0200

    Delete unique for payee

--- src/main/java/com/mlkb/ftm/entity/Payee.java
+++ src/main/java/com/mlkb/ftm/entity/Payee.java
@@ -16,7 +16,6 @@ public class Payee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(unique = true)
     private String name;
     @Enumerated(EnumType.STRING)
     @Column(length = 8)
