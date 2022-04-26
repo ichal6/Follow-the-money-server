99de4c93a616a1a05a1fcbe535f3d9dc5b67ace8
Michael Lechowicz <ichal.lechowicz@gmail.com>
2022-04-26 13:34:05 +0200

    Change greetings for test pull requests

--- src/main/java/com/mlkb/ftm/controller/TestController.java
+++ src/main/java/com/mlkb/ftm/controller/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class TestController {
     @GetMapping("/")
     public String greetings() {
-        return "Wejście Smoka. Test CI/CD";
+        return "Wejście Smoka. Pull request test";
     }
 }
