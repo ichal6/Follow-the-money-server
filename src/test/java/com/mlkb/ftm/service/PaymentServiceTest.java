383efd7bfc665842b594d8bf429b48eb8a2009e8
Michael Lechowicz <ichal.lechowicz@gmail.com>
2023-05-09 21:03:34 +0200

    Remove unused import

--- src/test/java/com/mlkb/ftm/service/PaymentServiceTest.java
+++ src/test/java/com/mlkb/ftm/service/PaymentServiceTest.java
@@ -20,7 +20,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.math.BigDecimal;
 import java.time.Clock;
 import java.time.Instant;
 import java.time.ZoneOffset;
