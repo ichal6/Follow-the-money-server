e384a42e0a995010b993227414cff14e04d89057
Michael Lechowicz <ichal.lechowicz@gmail.com>
2023-06-25 19:52:35 +0200

    Revert "Add Cookie for CORS"
    This reverts commit 6e3cf0d02935342d7ab643fa8dc9fbf246da355c.

--- src/main/java/com/mlkb/ftm/config/CORSFilter.java
+++ src/main/java/com/mlkb/ftm/config/CORSFilter.java
@@ -32,9 +32,7 @@ public class CORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me, Cookie");
-
-        this.log.info("CORS Allowed");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
 
         chain.doFilter(req, res);
     }
