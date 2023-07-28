fe08dd87809925034d2bcd1b0e66d9994358ab98
Michael Lechowicz <ichal.lechowicz@gmail.com>
2023-07-28 18:18:38 +0200

    Revert "Merge branch 'dev' into FTM-180-BE-Move-deploy-from-Railway-to-Mikrus"
    This reverts commit 886c465ef87889bc6a95c7bf848e8b521beee64f, reversing
    changes made to 369a4907ef6e3a3ec2dbb08e278fe952b6386de1.

--- src/test/java/com/mlkb/ftm/fixture/TransactionDTOFixture.java
+++ src/test/java/com/mlkb/ftm/fixture/TransactionDTOFixture.java
@@ -7,20 +7,6 @@ public class TransactionDTOFixture {
         final var transaction = new TransactionDTO();
         transaction.setId(1L);
         transaction.setTitle("Buy Car after edit");
-        transaction.setAccountId(AccountEntityFixture.millennium().getId());
-        transaction.setValue(-2400.0);
-        transaction.setType("Expense");
-
-        return transaction;
-    }
-
-    public static TransactionDTO buyCarTransactionReturn() {
-        final var transaction = new TransactionDTO();
-        transaction.setId(1L);
-        transaction.setTitle("Buy Car after edit");
-        transaction.setAccountId(AccountEntityFixture.millennium().getId());
-        transaction.setValue(2400.0);
-        transaction.setType("Income");
 
         return transaction;
     }
