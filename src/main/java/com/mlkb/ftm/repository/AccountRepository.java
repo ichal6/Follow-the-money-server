b66fbdd78dbbba80e62e6f8742538f3ef08b790f
Michael Lechowicz <ichal.lechowicz@gmail.com>
2023-07-28 18:10:46 +0200

    Revert "Add find by Account ID and User email"
    This reverts commit 5e6896a921428caba2fcd203fe8f3ea21b73d506.

--- src/main/java/com/mlkb/ftm/repository/AccountRepository.java
+++ src/main/java/com/mlkb/ftm/repository/AccountRepository.java
@@ -2,14 +2,8 @@ package com.mlkb.ftm.repository;
 
 import com.mlkb.ftm.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
-    @Query( "SELECT ac " +
-            "FROM User u INNER JOIN u.accounts ac WHERE ac.id = ?1 AND u.email = ?2")
-    Optional<Account> findByAccountIdAndUserEmail(Long Id, String email);
 }
